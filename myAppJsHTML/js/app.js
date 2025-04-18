2197621335f12a01c419e4ac0f994251328e9267
Cesar Baron <cesarbaronr@gmail.com>
2025-04-18 16:40:50 -0500

    CSS advance and also the creation inputs were added

--- myAppJsHTML/js/app.js
+++ myAppJsHTML/js/app.js
@@ -1,72 +1,82 @@
-console.log("mi primera connecion con js")
-
-
-const btn = document.getElementById("btnAlerta")
-
-btn.addEventListener("click", () => {alert("esto eta funcionando")})
-
-
-const mostrarAlerta = () =>{
- alert("esta funcionando")
+class Actividad {
+  constructor(titulo, descripcion, img, id) {
+    this.titulo = titulo;
+    this.descripcion = descripcion;
+    this.img = img;
+    this.id = id;
+  }
 }
 
+class Container {
+  constructor() {
+    this.container = [];
+  }
+
+  addActivity(titulo, descripcion, img, id) {
+    const myActivity1 = new Actividad(titulo, descripcion, img, id);
+    this.container.push(myActivity1);
+  }
+
+  showActivity() {
+    console.log(this.container);
+  }
+
+  deleteActivity(id) {
+    this.container = this.container.filter((x) => x.id != id);
+  }
+
+  filterActivity(activity) {
+    const result = (this.container = this.container.filter(
+      (x) => x.titulo == activity
+    ));
+    return result;
+  }
+}
+
+const newContainer = new Container();
+
+buttonAdd.addEventListener("click", () => {
+  addActivityhandler();
+});
+
+const addActivityhandler = () => {
+  const activityValue = document.getElementById("activity").value;
+  const descriptionValue = document.getElementById("description").value;
+  const linkValue = document.getElementById("link").value;
+  const faltan = document.getElementById("faltanDatos")
+
+  if (validateImput(activityValue, descriptionValue, linkValue) === false) {
+    faltan.innerHTML = "Missing info"
+  } else newContainer.addActivity(
+    activityValue,
+    descriptionValue,
+    linkValue,
+    crypto.randomUUID,
+    faltan.innerHTML = ""
+  );;
+
+showItem() 
 
 
-    class Actividad {
-        constructor(titulo,descripcion,img,id ) {
-        this.titulo = titulo;
-        this.descripcion = descripcion;
-        this.img = img;
-        this.id = id;
-        
-        }
-    }
-
-    class Container {
-        constructor() {
-            this.container = []
-        }
-
-    addActivity(titulo,descripcion,img,id){
-        const myActivity1 = new Actividad(titulo,descripcion,img,id);
-        this.container.push(myActivity1)
-    }
-
-    showActivity(){
-        console.log(this.container)
-        
-    }
-
-    deleteActivity(id){
-    this.container = this.container.filter((x) => x.id != id)
-    }
-
-    filterActivity(activity){
-        const result = this.container = this.container.filter((x) => x.titulo == activity)
-        return result
-    }
-    }
+  console.log("addactivity",newContainer.container[0])
+};
 
 
 
-    const newContainer = new Container() 
+// const showItem = () =>{
+// const newActivity = document.getElementById("NewActivity")
+// const containerActivity = document.querySelector(".newActivity")
+// const actividad = containerActivity.createElement('div')
+// actividad.innerHTML = "hola"
 
-    
-newContainer.addActivity("pesca2","pescar Peces", "IMG", 1)
-// newContainer.addActivity("pesca3","pescar Peces", "IMG", 1)
-// newContainer.addActivity("pesca4","pescar Peces", "IMG", 1)
-// newContainer.addActivity("pesca5","pescar Peces", "IMG", 1)
-// newContainer.addActivity("pesca2","pescar Peces", "IMG", 1)
-// newContainer.addActivity("pesca2","pescar Peces", "IMG", 1)
-    
-// newContainer.deleteActivity(2)
+// }
 
+const validateImput = (a, b, c) => {
+  if (a == "" || b == "" || c == "") {
+    return false;
+    // create a label P to let him know that a part of data is missing
+  }
+  return true;
+};
 
-    
-newContainer.filterActivity("pesca2")
-
-
-newContainer.showActivity()
-
-
-module.exports = { Actividad, Container };
+module.exports = { Actividad, Container};
